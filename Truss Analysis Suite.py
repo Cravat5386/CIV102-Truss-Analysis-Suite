6c0f2afb27373648c332d903e0d596840e4dbcdd
Cravat5386 <56939620+Cravat5386@users.noreply.github.com>
2019-10-24 19:45:01 -0400

    Merge
    Merging virtual work and truss analysis suite

--- Truss Analysis Suite.py
+++ Truss Analysis Suite.py
@@ -3,9 +3,12 @@ from math import *
 Functions list:
 truss_analysis
 force_and_length
+virtual_work
 max_force
 lengths
 hss_constraints
+is_valid
+dummy_forces
 """
 #_______________________________________________________________________________________________________________________________________________#
 
@@ -192,6 +195,18 @@ def force_and_length(joints_number, joint_load, deg1, deg2, length, orientation 
       tplace.append(tnew)
   return ls, [cplace, tplace]
 
+def virtual_work(member,force,area,modulus,dummy_forces):
+
+    '''
+    Uses the length of the member (member), the force acting on it (force),
+    the member's Young's modulus, and the dummy forces on each member to
+    Calculate the vertical deformation of the bridge (total)
+    '''
+    total = 0
+    for i in range(len(member)):
+        total += dummy_forces[i]*(force[i]*member[i])/(modulus*area[i])
+    return total
+
 def max_force(ls, orientation = "top"):
   """
   (list, string) -> list
@@ -283,3 +298,93 @@ def hss_constraints(force, length, direction = "tension"):
   else:
     print("A > " + str(trunc(2 * force * 1000 / 350)) + " mm2")
     print("I > " + str(trunc(3 * 1000000 * force*length**2/(200000*pi**2)) / 1000) + "*10^6 mm4")
+
+def is_valid(member,force):
+    '''
+    check if input is valid
+    (list,list)->boolean
+    is_valid([1,2,3,4],[5,6,6])
+    >>>0
+    '''
+    if len(member)!=len(force):
+        return 0
+    return 1
+
+def dummy_forces(joints_number, deg1, deg2, orientation = "top"):
+#Still have to check to see if it works.
+  """
+  (num, num, num, string) -> list
+  Given a truss, finds the dummy forces for the method of virtual work.
+  """
+  ten = []
+  com = []
+  if orientation == "top":
+    a = 1
+    b = -1
+  else:
+    a = -1
+    b = 1
+  theta1 = radians(deg1)
+  theta2 = radians(deg2)
+  if joints_number % 2 == 0:
+    n = joints_number / 2 - 1
+    ran = (joints_number + 1) // 4
+    error = 1
+  else:
+    n = (joints_number - 3) / 2
+    ran = (joints_number + 1) // 4
+    error = joints_number % 4 - 1
+  rxn = 1 / 2
+  f1 = rxn / sin(theta1)
+  if b == 1:
+    ten.append(f1)
+  else:
+    com.append(f1)
+  f2 = rxn / sin(theta1) * cos(theta1)
+  if a == 1:
+    ten.append(f2)
+  else:
+    com.append(f2)
+  f3 = f1 * sin(theta1) / sin(theta2)
+  if a == 1:
+    ten.append(f3)
+  else:
+    com.append(f3)
+  f4 = f1 * cos(theta1) + f3 * cos(theta2)
+  if b == 1:
+    ten.append(f4)
+  else:
+    com.append(f4)
+  for i in range(ran-1):
+    f1 = f3 * sin(theta2) / sin(theta1)
+    if b == 1:
+      ten.append(f1)
+    else:
+      com.append(f1)
+    f2 = f2 + f3 * cos(theta2) + f1 * cos(theta1)
+    if a == 1:
+      ten.append(f2)
+    else:
+      com.append(f2)
+    f3 = f1 * sin(theta1) / sin(theta2)
+    if a == 1:
+      ten.append(f3)
+    else:
+      com.append(f3)
+    f4 = f4 + f1 * cos(theta1) + f3 * cos(theta2)
+    if b == 1:
+      ten.append(f4)
+    else:
+      com.append(f4)
+  if joints_number % 2 == 0:
+    com.pop()
+    ten.pop()
+    if a == 1:
+      ten.append(1)
+    else:
+      com.append(1)
+  else:
+    if error == 2:
+      com.pop()
+      ten.pop()
+  return [com, ten]
