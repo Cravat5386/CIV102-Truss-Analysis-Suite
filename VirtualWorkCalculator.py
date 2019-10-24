8217a195844b0315f80cea3e57ad40d288f0f23c
eqd11 <ericdai1130@gmail.com>
2019-10-23 23:07:02 -0400

    rev 2.0
    completed algorithm
    wrote docstring for is_valid
    need to come up with examples with second one

--- VirtualWorkCalculator.py
+++ VirtualWorkCalculator.py
@@ -4,11 +4,25 @@ def is_valid(member,force):
         (member,force)=float(input("Invalid! Please re-enter length of member and force acting:"))
         is_valid(member,force)
     '''
+    '''
+    check if input is valid
+    (list,list)->boolean
+    is_valid([1,2,3,4],[5,6,6])
+    >>>0
+    '''
     if len(member)!=len(force):
         return 0
     return 1
-def virtual_work(member,force,area,modulus):
+
+def virtual_work(member,force,area,modulus,dummy_forces):
+    work=[0 for i in range(len(member))]
     for i in range(member):
         deform[i]=(force[i]*member[i])/(modulus*area[i])
-    #if we could change kevin's program and make them return values this part would be easy to apply truss analysis
+         #if we could change kevin's program and make them return values this part would be easy to apply truss analysis
+        work[i]=dummy_forces[i]*deform[i]
+    for j in range(work):
+        total+=work[i]
+    ver_deform=total
+    return ver_deform
+        
     
